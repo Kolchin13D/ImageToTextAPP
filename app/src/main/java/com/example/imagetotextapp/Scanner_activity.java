c64bffde182a075f48f55b944415116b398f4454
Kolchin13D <kolchin13d@gmail.com>
2024-07-18 16:49:58 +0300

    testing

--- app/src/main/java/com/example/imagetotextapp/Scanner_activity.java
+++ app/src/main/java/com/example/imagetotextapp/Scanner_activity.java
@@ -1,16 +1,21 @@
 package com.example.imagetotextapp;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -59,11 +64,32 @@ public class Scanner_activity extends AppCompatActivity {
     }
 
     private void captureImage() {
+
+        Intent takeImage = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takeImage.resolveActivity(getPackageManager()) != null){
+            startActivityForResult(takeImage, REQUEST_IMAGE_CAPTURE);
+        }
     }
 
     private void requestPermission() {
         int code = 200;
-        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.});
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, code);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (grantResults.length > 0){
+            boolean cameraPermissiom = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+            if (cameraPermissiom){
+                Toast.makeText(this, "Permission Granted ", Toast.LENGTH_SHORT).show();
+                captureImage();
+            }else{
+                Toast.makeText(getApplicationContext(), "Permission denied !", Toast.LENGTH_SHORT).show();
+            }
+        }
+
     }
 
     private void RecognitionText() {
