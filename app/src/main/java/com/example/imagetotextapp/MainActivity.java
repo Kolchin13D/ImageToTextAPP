6f46f012ed5b25169e9e2c43b5ddf7574c4dbd6d
Kolchin13D <kolchin13d@gmail.com>
2024-07-18 16:38:44 +0300

    testing

--- app/src/main/java/com/example/imagetotextapp/MainActivity.java
+++ app/src/main/java/com/example/imagetotextapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.imagetotextapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,12 +13,20 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button captureBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-
+        captureBtn = findViewById(R.id.newScan);
+        captureBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Scanner_activity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/imagetotextapp/Scanner_activity.java
+++ app/src/main/java/com/example/imagetotextapp/Scanner_activity.java
@@ -1,24 +1,71 @@
 package com.example.imagetotextapp;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class Scanner_activity extends AppCompatActivity {
+
+    private ImageView captureIV;
+    private TextView resultTV;
+    private Button snapbtn, recogBtn;
+    private Bitmap imageBitmap;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_scanner);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        captureIV = findViewById(R.id.CaptureImage);
+        resultTV = findViewById(R.id.textView);
+        snapbtn = findViewById(R.id.snapBtn);
+        recogBtn = findViewById(R.id.recogBtn);
+
+        recogBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RecognitionText();
+            }
         });
+
+        snapbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(checkPermission()){
+                    captureImage();
+                }else{
+                    requestPermission();
+                }
+            }
+        });
+    }
+
+    private boolean checkPermission() {
+        int camera_permisiion = ContextCompat.checkSelfPermission(getApplicationContext(), CAMERA_SERVICE);
+        return camera_permisiion == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void captureImage() {
+    }
+
+    private void requestPermission() {
+        int code = 200;
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.});
+    }
+
+    private void RecognitionText() {
     }
 }
